d4dec853e793ed7cf411561fe052cd4a675a25d6
Gayal Shamane <shamane92siriwardhana@gmail.com>
2017-06-30 17:24:54 +0530

    Update attn_gru.py

--- utils/attn_gru.py
+++ utils/attn_gru.py
@@ -7,29 +7,33 @@ class AttnGRU:
     """Attention-based Gated Recurrent Unit cell (cf. https://arxiv.org/abs/1603.01417)."""
 
     def __init__(self, num_units, is_training, bn):
-        self._num_units = num_units
+        self._num_units = num_units #number of units in the GRU
         self.is_training = is_training
-        self.batch_norm = bn
-
-    def __call__(self, inputs, state, attention, scope=None):
+        self.batch_normx = bn
+  
+ 
+    def __call__(self, inputs, state, attention, scope=None): #this can run with the object we created 
+#inputs are the fution layer outputs  at each time steps 
+#states are the hidden states that out from the GRU
+#attention is the new update gate..
         """Gated recurrent unit (GRU) with nunits cells."""
         with tf.variable_scope(scope or 'AttrGRU'):
             with tf.variable_scope("Gates"):  # Reset gate and update gate.
                 # We start with bias of 1.0 to not reset.
-                r = tf.nn.sigmoid(self._linear(inputs, state, bias_default=1.0))
+                r = tf.nn.sigmoid(self._linear(inputs, state, bias_default=1.0))  #the reset gate the in modified GRU
             with tf.variable_scope("Candidate"):
-                c = tanh(self._linear(inputs, r * state))
+                c = tanh(self._linear(inputs, r * state))  #intermidiate state 
 
-            new_h = attention * c + (1 - attention) * state
-        return new_h
+            new_h = attention * c + (1 - attention) * state #this is the new hidden state 
+        return new_h      #return the output function 
 
-    def _linear(self, x, h, bias_default=0.0):
+    def _linear(self, x, h, bias_default=0.0):  #this is to multiply the internal things with gates 
         I, D = x.get_shape().as_list()[1], self._num_units
         w = weight('W', [I, D])
         u = weight('U', [D, D])
         b = bias('b', D, bias_default)
 
-        if self.batch_norm:
+        if self.batch_normx:  #batch norm
             with tf.variable_scope('Linear1'):
                 x_w = batch_norm(tf.matmul(x, w), is_training=self.is_training)
             with tf.variable_scope('Linear2'):
